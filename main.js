ac910cfa391470d701778b69133b7fe96b982cf4
Mathias <mathiasabelandersen@gmail.com>
2024-11-12 13:51:26 +0100

    Getallcars implementert, html og java virker.

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
+const baseUri = "http://localhost:5180/api/Cars"
+
 const app = Vue.createApp({
     data() {
         return {
             intro: 'Welcome to my Vue template',
+            carList:[]
         }
     },
     methods: {
-        myMethod(){
+        getAllCars(){
+            axios.get(baseUri)
+            .then
+            (
+                response => 
+                {
+                    console.log(response)
+                    this.carList = response.data
+
+                }
+            )
+            .catch 
+            (
+                error => 
+                {
+                console.log(error)
+                }    
+            )
+                
+            
+            
+               
 
         },
     },
